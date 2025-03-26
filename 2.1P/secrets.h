ade06219bedd9fd1090cd4be91893d6e8f6d0011
PatrickHadjis1324 <s222236157@deakin.edu.au>
2025-03-26 15:26:30 +1100

    Add files via upload

---
+++ 2.1P/secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "Optus_C03f12"
+#define SECRET_PASS "kappa62737ws"
+#define SECRET_CH_ID 2889758       // Replace with your ThingSpeak Channel ID
+#define SECRET_WRITE_APIKEY "2AE91EN72FCKBQCD"
